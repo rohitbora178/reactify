e9511ba5a4360f31dce4438b1d5a7f4ce84cb90f
rohitbora178 <rohitbora178@gmail.com>
2026-02-13 20:58:40 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1 className="title">Hi Navinya 👋</h1>
+        <p className="subtitle">Welcome to my awesome animated page 🚀</p>
+        <button className="btn">Click Me</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
